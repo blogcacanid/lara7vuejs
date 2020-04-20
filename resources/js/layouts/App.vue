a1637b69beb440bfdf969968e7d2cc0706f9106a
rokib <akun.rokib.id@gmail.com>
2020-04-20 11:30:02 +0700

    publish file index

---
+++ resources/js/layouts/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <!-- navbar -->
+        <nav class="navbar navbar-expand-sm navbar-light bg-white shadow-sm">
+            <a href="#" @click.prevent="" class="navbar-brand">Lara7vuejs</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link class="nav-link" :to="{ name:'home' }">
+                            Home
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" :to="{ name:'about' }">
+                            About
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" :to="{ name:'contact' }">
+                            Contact
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <!-- content -->
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
---
+++ resources/js/router.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Contact from './views/Contact.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: Contact
+        }
+    ]
+})
+
+export default router
